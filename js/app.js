e8a0e84a8a3112777f16a92b25251a84378d0874
left-204 <22110040@oic-ok.ac.jp>
2024-09-03 10:33:49 +0900

    更新

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
+//シャッフルして山札生成
 const apiUrl = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 let deck_id ="";
 let apiUrl2 = "";
 document.getElementById("get").addEventListener("click", async () => {
     let response = await fetch(apiUrl);
+    //jsの型に変換
     let trump = await response.json();
     console.log(trump.deck_id);
+
+    //deck_idを取得して一枚引く
     deck_id = trump.deck_id;
-    apiUrl2 = "https://www.deckofcardsapi.com/api/deck/"+ deck_id +"/draw/?count=11";
+    apiUrl2 = "https://www.deckofcardsapi.com/api/deck/"+ deck_id +"/draw/?count=2";
     console.log(apiUrl2)
     let response2 = await fetch(apiUrl2);
+        //カードを2枚引いたやつをjs型に変更
     let trump2 = await response2.json();
     console.log(trump2.cards[0]);
+    //imgタグを生成
     let card_img = document.createElement("img");
+    //imgタグにセット
     card_img.src = trump2.cards[0].image;
     let disp = document.getElementById("display");
+    //表示
     disp.appendChild(card_img);
 });
 
