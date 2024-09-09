c4df153c280198240440ff4849081102f660bb25
k-araki <22110014@oic-ok.ac.jp>
2024-09-09 14:22:45 +0900

    捕獲される機能

---
+++ js/card_capture/captured.js
@@ -0,0 +1,35 @@
+let oppo_capture = [];
+function captured() {
+    let player_card_0 = document.getElementById("player_card_0_box");
+    let player_card_1 = document.getElementById("player_card_1_box");
+    let player_card_2 = document.getElementById("player_card_2_box");
+    let player_card_3 = document.getElementById("player_card_3_box");
+    let oppo_card_0 = document.getElementById("oppo_card_0_box");
+    if(player_card_0.checked){
+        console.log(player_card[0].code);
+    }else{
+        console.log("チェックされてないよ");
+    }
+    if(player_card_1.checked){
+        console.log(player_card[1].code);
+    }else{
+        console.log("チェックされてないよ");
+    }
+    if(player_card_2.checked){
+        console.log(player_card[2].code);
+    }else{
+        console.log("チェックされてないよ");
+    }
+    if(player_card_3.checked){
+        console.log(player_card[3].code);
+    }else{
+        console.log("チェックされてないよ");
+    }
+    
+    console.log("敵側");
+    if(oppo_card_0.checked){
+        console.log(oppo_card[0].code);
+    }else{
+        console.log("チェックされてないよ");
+    }
+  }
