c6364bc79b47b732b9824f9e3457bf30ec4b62f1
left-204 <22110040@oic-ok.ac.jp>
2024-09-12 08:21:50 +0900

    調整

---
+++ js/card_capture/disp_system.js
@@ -0,0 +1,12 @@
+$('#player_card_0_box').change(function(){
+    
+});
+$('#player_card_1_box').change(function(){
+
+});
+$('#player_card_2_box').change(function(){
+
+});
+$('#player_card_3_box').change(function(){
+
+});
